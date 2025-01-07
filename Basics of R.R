c12dc0db60c15c44df482c263cb01a3924675cb9
rushikeshdanej <rushikeshdanej123@gmail.com>
2025-01-07 17:28:48 +0530

    Add files via upload

---
+++ Basics of R.R
@@ -0,0 +1,38 @@
+75*765
+
+
+number = 57375
+
+
+number*2
+
+#Variables
+#Variables should always be created with letters or underscore
+#Variables should not contain special character
+#Numbers should not be used at start. 
+#Case sensitive
+#Variable name should be meaningful
+
+abc = 75.5
+ab!c= 657684
+9the = 65
+the9 = 85
+
+
+Apple = 78
+apple = 78
+
+
+
+abc = 45
+class(abc)
+xyz = 75.36
+class(xyz)
+name = 'Nikita'
+class(name)
+logic = FALSE
+class(logic)
+
+data = datasets::airquality
+
+rm(abc)
